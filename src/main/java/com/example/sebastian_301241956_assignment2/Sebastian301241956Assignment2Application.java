324e760daf1fe0bf02b4bb4306034a7a88ff0684
Sebastian <sebastian_bdavid@hotmail.com>
2025-02-17 14:45:34 -0500

    init project

---
+++ src/main/java/com/example/sebastian_301241956_assignment2/Sebastian301241956Assignment2Application.java
@@ -0,0 +1,13 @@
+package com.example.sebastian_301241956_assignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sebastian301241956Assignment2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sebastian301241956Assignment2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sebastian_301241956_assignment2/Sebastian301241956Assignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sebastian_301241956_assignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sebastian301241956Assignment2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
